496ad64862b0984c06eeed31d538889b78a26f78
Huỳnh Lâm Đăng Khoa <khoahuynhlamdang@gmail.com>
2024-11-26 11:49:12 +0700

    Default Structure

---
+++ src/fluxio/Fluxio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fluxio;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Fluxio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
