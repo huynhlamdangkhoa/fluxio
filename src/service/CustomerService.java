ba3b8f4aeb8f3a517c0fa3777ce6972efe079a3d
Krew <113541251+Krewella03@users.noreply.github.com>
2024-12-20 20:17:04 +0700

    Update CustomerService.java

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,4 +1,3 @@
-// CustomerDAO.java
 package dao;
 
 import model.Customer;
@@ -9,57 +8,47 @@ public interface CustomerDAO extends GenericDAO<Customer, Long> {
     List<Customer> findByName(String name);
 }
 
-// CustomerService.java
 package service;
 
 import dao.CustomerDAO;
 import model.Customer;
 import java.util.List;
 
-public class CustomerService {
+public class CustomerService{
     private final CustomerDAO customerDAO;
-
-    public CustomerService(CustomerDAO customerDAO) {
+    public CustomerService(CustomerDAO customerDAO){
         this.customerDAO = customerDAO;
     }
-
-    public boolean addCustomer(Customer customer) {
-        if (isValidCustomer(customer)) {
+    public boolean addCustomer(Customer customer){
+        if (isValidCustomer(customer)){
             customerDAO.insert(customer);
             return true;
         }
         return false;
     }
-
-    public boolean updateCustomer(Customer customer) {
-        if (customer != null && customer.getId() != null) {
+    public boolean updateCustomer(Customer customer){
+        if (customer != null && customer.getId() != null){
             customerDAO.update(customer);
             return true;
         }
         return false;
     }
-
-    public boolean deleteCustomer(Long id) {
+    public boolean deleteCustomer(Long id){
         return customerDAO.delete(id) > 0;
     }
-
-    public List<Customer> getAllCustomers() {
+    public List<Customer> getAllCustomers(){
         return customerDAO.findAll();
     }
-
-    public Customer getCustomerById(Long id) {
+    public Customer getCustomerById(Long id){
         return customerDAO.findById(id);
     }
-
-    public Customer getCustomerByEmail(String email) {
+    public Customer getCustomerByEmail(String email){
         return customerDAO.findByEmail(email);
     }
-
-    public List<Customer> getCustomersByName(String name) {
+    public List<Customer> getCustomersByName(String name){
         return customerDAO.findByName(name);
     }
-
-    private boolean isValidCustomer(Customer customer) {
+    private boolean isValidCustomer(Customer customer){
         return customer != null
             && customer.getName() != null && !customer.getName().isEmpty()
             && customer.getEmail() != null && customer.getEmail().contains("@");
